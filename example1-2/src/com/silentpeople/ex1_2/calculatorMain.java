61dda01a41c08e20c854ed46c1e244b7595bf995
honbob <honbob123@ruu.kr>
2016-11-16 20:05:56 +0900

    2016-11-16 20:05 수정

--- example1-2/src/com/silentpeople/ex1_2/calculatorMain.java
+++ example1-2/src/com/silentpeople/ex1_2/calculatorMain.java
@@ -18,18 +18,44 @@ public class calculatorMain {
 		}
 		int [] num = {0,0,0,0,0,0,0,0,0};
 		for(int cnt=0; cnt < chkvals.length; cnt++){
-			 
-			num[cnt] = Integer.parseInt(chkvals[cnt]);
-			System.out.println(num[cnt]);
+			try{
+				num[cnt] = Integer.parseInt(chkvals[cnt]);
+				System.out.println(num[cnt]);
+			}catch(java.lang.NumberFormatException e){
+				System.out.println("입력범위 초과: -2147483648 ~ 2147483648");
+			}
 		}
 		
-		String [] sign = {"+", "-", "*", "/"};
-		for(int cnt = 0; cnt < sign.length; cnt++){
-			int signOrder = inputVal.indexOf(sign[cnt]);
+		String [] signArray = {"+", "-", "*", "/"};
+		char sign = '0';
+		for(int cnt = 0; cnt < signArray.length; cnt++){
+			int signOrder = inputVal.indexOf(signArray[cnt]);
+			if(signOrder < 0){
+				continue;
+			}
 			System.out.println(signOrder);
+			sign = inputVal.charAt(signOrder);
+			System.out.println(sign);
 		}
 		
-		
+		switch(sign){
+		case '+':
+			long result = Long.valueOf(num[0]) + (long)num[1];
+			System.out.println(result);
+			break;
+		case '-':
+			long result1 = num[0] - num[1];
+			System.out.println();
+			break;
+		case '*':
+			System.out.println(num[0] * num[1]);
+			break;
+		case '/':
+			System.out.println(num[0] / num[1]);
+			break;
+		default:
+			break;
+		}
 	
 		
 		
